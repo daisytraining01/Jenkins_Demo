d5755f181d00736af832d10aeb00599197e9944b
ParkaviTerrain <mavericterrain@gmail.com>
2020-05-13 08:33:18 +0530

    final demo

---
+++ src/test/java/parkavi/PatientConsultation.java
@@ -0,0 +1,138 @@
+package parkavi;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class PatientConsultation extends PatientDetails implements PatientManagement{	
+	 
+	
+	public static void main(String[] args) {
+		/*System.out.println("*********Patient Management System-ERP********");		
+		System.out.println("Enter the patient details:");
+		Scanner s=new Scanner(System.in);
+		 System.out.print("Enter the patient name:");			
+		 String patientName=s.nextLine();
+		 System.out.print("Enter the patient contact number:");			
+		 int contactNumber=s.nextInt();
+		 System.out.print("Enter the patient gender:");			
+		 String gender=s.next();
+		 System.out.print("Enter the patient age:");
+		 int age=s.nextInt();
+		 System.out.print("Enter the patient Blood group:");			
+		 String BG=s.next();	
+		 System.out.print("Enter the patient ID proof:");			
+		 String IDProof=s.next();
+		 System.out.print("Enter the Doctor Name:");*/
+		
+		PatientConsultation pc=new PatientConsultation();
+		pc.setPatientName("parkavi");
+		pc.setGender("Female");
+		pc.setAge(25);
+		pc.setContactNumber(123435);
+		pc.setBloodGroup("A");
+		pc.setIDProof("Pan");
+		
+		pc.getPatientName();
+		pc.getGender();
+		pc.getAge();
+		pc.getBloodGroup();
+		pc.getContactNumber();
+		pc.getIDProof();
+		
+		pc.AppoitmentDetails();
+		pc.payment();
+		
+	}
+	
+	
+	public void AppoitmentDetails() {
+		Date date=new Date();
+		System.out.println("Current date and time - "+date);
+		}
+
+	/*Scanner s1=new Scanner(System.in);	*/
+	
+	
+	public String DoctorAvailablity() {
+		
+		String available="Yes";
+		String doctorName="saravanan";		
+		System.out.println("The doctor is available");
+		
+		return available;
+	}
+
+	public String LabTestResult() {
+		
+		String LabResult="";
+		if(this.DoctorAvailablity().equals("Yes")) {
+			
+		System.out.println();
+		LabResult="positive";
+		System.out.println("Here is your Test Result...");
+		}else {
+			LabResult="negative";
+			System.out.println("Doctor is not available ...your appointment will be rescheduled");
+		}
+		return LabResult;
+		
+	}
+
+	public String DoctorConsultation() {
+		
+		String prescription="";
+		if(this.LabTestResult().equals("positive")) {			
+			prescription = "DOLO650";
+			System.out.println("Doctor has prescribed "+prescription);
+		}
+		return prescription;
+		
+	}
+
+	
+	public boolean MedicinesPrescribed() {
+		
+		ArrayList<String> ls=new ArrayList<String>();
+		ls.add("DOLO650");
+		ls.add("Crosin");
+		ls.add("Citrosin");
+		ls.add("Aspernt");
+		ls.add("Paracitamol");
+		ls.add("Piriton");
+		
+		String DoctorPrescription=this.DoctorConsultation();
+		ls.contains(DoctorPrescription);
+		
+		if(this.DoctorAvailablity().equals("No")) {	
+			
+		}else if(ls.contains(DoctorPrescription)) {
+			System.out.println("Medicines are provided");
+		}else {
+			System.out.println("Medicines are not available");
+		}
+		return ls.contains(DoctorPrescription);
+	}
+	
+	public void payment() {
+		/*System.out.print("Enter the patient type :");
+		Scanner s4=new Scanner(System.in);
+		String TypeOfPatient=s4.next();*/
+				
+		if(this.MedicinesPrescribed()==true&&
+				this.DoctorAvailablity().equals("Yes")) {					
+		
+		int patientAccountBalance=3450;		
+		int MedicineFee=70;
+		int DoctorFee=350;
+		int Amount=0;	
+		 
+	    Amount=patientAccountBalance-(MedicineFee+DoctorFee);
+	    System.out.println("Patients Existing balance- "+patientAccountBalance);		
+		System.out.println("Doctor fee- "+DoctorFee+ " MedicineFee- "+MedicineFee);
+		System.out.println("Patient Account balance after debit- "+Amount);
+		System.out.println("******Payment has been made sucessfully*****");
+		
+	}
+	}
+}
---
+++ src/test/java/parkavi/PatientManagement.java
@@ -0,0 +1,18 @@
+package parkavi;
+
+public interface PatientManagement {
+	
+	void AppoitmentDetails();
+	
+	String DoctorAvailablity();
+	
+	String LabTestResult();
+		
+	String DoctorConsultation();
+	
+	boolean MedicinesPrescribed();
+	
+	void payment();	
+	
+
+}
